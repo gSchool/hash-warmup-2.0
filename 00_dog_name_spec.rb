2990dc46d8cc9ce2ffd2ac50af3e6ba937a75884
Kinsey Durham <kinseyann505@gmail.com>
2014-03-27 14:52:48 -0600

    First spec

--- 00_dog_name_spec.rb
+++ 00_dog_name_spec.rb
@@ -2,4 +2,9 @@ require 'rspec'
 require 'rspec/autorun'
 
 describe 'Dog Hash' do
+  it "returns the dog's first name" do
+    expect(DOG[:first_name]).to eq ("Harleigh")
+  end
 end
+
+#Implement the solution here:
